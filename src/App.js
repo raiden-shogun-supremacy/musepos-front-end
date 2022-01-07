86841b05bc3063964547b90d002305a286f6d6ac
เจษฎา จินะกะ <Jetsada_Jinaka@hotmail.com>
2022-01-07 16:03:26 +0700

    Regist2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import './style.css';
 import './Components/Login/Login.css';
+import Register from 'D:/SEPROJECT/New folder/musepos-front-end/src/Components/Register/Register.js';
+/*import Login from './Components/Login/Login.js';*/
 
 function App() {
   return (
-    <div>
-      <div className="middle">
-        <img src="..\Aeests\img\logo.png" className="logo" />
-        <p className="light">Make your resturant easier</p>
-      </div>
-      <div className="middle">      
-        <input className="ma-29" type='text' name='Username' />
-        <input className="ma-29" type='text' next='Passward' />
-      </div>
-      <div className="middle">
-        <div class="mt-64">
-          <button class="button2">Log in!</button>
-        </div>
-        <div className="mt-34">
-          <p>Or no have User ID? Register new one!</p>
-        </div>
-      </div>
+    <div className="middle">
+    <Register />  
+      
     </div>
-  );
+    
+    
+);
 }
 
 export default App;
---
+++ src/Components/Register/Register.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import '../../style.css'
+import 'D:/SEPROJECT/New folder/musepos-front-end/src/Components/Register/Register.css'
+
+/*import Login from '../Login/Login.js';*/
+
+ 
+function Register() {
+    
+    return (
+        <div>
+           
+           <h1 className='Re'> Register</h1> 
+           <h5>
+            <div>
+                <p className="Le">Let's earn money together!</p> 
+                <p className="We">Can we know you?</p> 
+            </div>
+            
+                 
+            
+            </h5>
+            
+            <form>
+                <div>
+                    <input type="text"  id="Firstname" />
+                </div>
+
+                    <div>
+                        <input type="text"  id="Lastname" />
+                    </div>
+
+                    <div>
+                        <input type="text"  id="PhoneNum" />
+                    </div>
+
+                    <div>
+                        <input type="text"  id="Username" />
+                    </div>
+
+                    <div>
+                        <input type="text"  id="Password" />
+                    </div>
+            </form>
+            <button id="RegisBut">Register</button>
+            
+            <img src="..\Aeests\img\BG.png" className='BG'  />
+            <img src="..\Aeests\img\BG2.png" className='BG2'  />
+            
+            
+           
+        </div>
+        
+           
+    )
+
+}
+
+export default Register
