cf1b393bb641cb54ed978dfaa4d82289b4c0941d
เจษฎา จินะกะ <Jetsada_Jinaka@hotmail.com>
2022-01-22 23:46:08 +0700

    RegisterFixed

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,44 +1,76 @@
 import '../style.css'
-import './Register.css'
+import styled from 'styled-components';
+
+
+const Form = styled.form`
+display:flex;
+flex-direction: column;
+gap: 19px;   
+margin-top: auto;
+margin-bottom: 80px;
+justify-content: center;
+max-width:auto ;
+`
+const Head = styled.div`
+margin-top:102px;
+display:grid;
+flex-direction:column;
+margin-left:16px
+`
+const Regis = styled.h1`
+font-size: 42px;
+border-style: none;
+margin-bottom: -30px;
+font-weight: 700;
+`
+
+const Let = styled.h6`
+font-size: 14px;
+line-height: 16px;
+font-weight: 500;
+`
+const Can = styled.h6`
+font-size: 14px;
+line-height: 16px;
+font-weight: 500;
+margin-bottom: 19px;
+
+@media (max-width: 768px) {
+    width:100;
+  }
+
+`
+const Div1 = styled.div`
+display:flex;
+flex-direction:row;
+gap: 20px;
+
+@media (max-width: 960px) {
+    flex-direction:column;
+  }
+`
+
+
+
 
 function Register() {
     return (
-        <div className='middle'>
-           
-           <h1> Register</h1> 
-           <h5>
-            <div>
-                <p className="">Let's earn money together!</p> 
-                <p className="">Can we know you?</p> 
-            </div>
-            
-            </h5> 
-
-            <div className='Form'>
-                <div >
-                    <input type="text"  id="" placeholder='Firstname' />
-                </div>
-                    <div>
-                        <input type="text"  id="" placeholder='Lastname' />
-                    </div>
-                    <div>
-                        <input type="text"  id="" placeholder='PhoneNum'/>
-                    </div>
-                    <div>
-                        <input type="text"  id="" placeholder='Username'/>
-                    </div>
-                    <div>
-                        <input type="text"  id="" placeholder='Password'/>
-                    </div>
-                   
-            </div>     
-            <button>Register</button>
-            
-            
-                       
-        </div>
-        
-           
+        <Head>
+                
+                <Regis>Register</Regis> 
+                <Let>Let's earn money together!</Let>  
+                <Can>Can we know you?</Can> 
+            <Form>
+                <Div1>
+                <input type="text"  placeholder='Firstname' />
+                <input type="text"  placeholder='Lastname' />
+                </Div1>                
+                <input type="text"  placeholder='Phone Number' />
+                <input type="text"  placeholder='Username' />
+                <input type="text"  placeholder='Password' />   
+            </Form>
+            <button>Register</button>     
+        </Head>        
     );
 
 }
